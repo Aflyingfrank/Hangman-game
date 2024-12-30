cda6010b74d503cd4e8a9d3748c0a3643de38a6e
Aflyingfrank <frank_zhongsh@outlook.com>
2024-12-30 22:09:00 +0800

    Add files via upload

--- hangman.R
+++ hangman.R
@@ -1,151 +1,132 @@
 library(ggplot2)
 
-zamenjaj2 <- function(beseda, crka, iskana_beseda){
-  
-  if (regexpr(crka, beseda)[1] > 0) {
+# Function to update the guessed word based on the correct guess
+updateGuessedWord <- function(word, letter, guessedWord) {
+  if (regexpr(letter, word)[1] > 0) {
+    word <- tolower(word)
+    letter <- tolower(letter)
+    positions <- which(strsplit(word, "")[[1]] == letter)
+    guessedWord[positions] <- letter
+    message(paste(guessedWord, collapse = " "))
     
-    beseda <- base::tolower(beseda)
-    crka <- base::tolower(crka)
-    
-    pozicije <- which(strsplit(beseda, "")[[1]]==crka)
-    iskana_beseda[pozicije] <- crka
-    
-    message(paste(iskana_beseda, collapse =  " "))
-    
-    if (paste(iskana_beseda, collapse = "") == beseda) {
-      return(iskana_beseda)
-      message("Game Over...")
-}
-    
-    return(iskana_beseda)
-}
+    if (paste(guessedWord, collapse = "") == word) {
+      message("Congratulations! You guessed the word!")
+      return(guessedWord)
+    }
+    return(guessedWord)
+  }
 }
 
-drawHead <- function(orig_position = c(0,0),
-                     dia = 1,
-                     nof_points = 10,
-                     group = 5){
-  
-  vectT <- seq(0,2*pi, length.out = nof_points)
-  r <- dia/2
-  x_data <- orig_position[1] + r * cos(vectT)
-  y_data <- orig_position[2] + r * sin(vectT)
-  return(data.frame (x = x_data, y = y_data, group = group))
+# Function to draw the head of the hangman
+drawHead <- function(origin = c(0, 0), diameter = 1, numPoints = 10, group = 5) {
+  angleSeq <- seq(0, 2 * pi, length.out = numPoints)
+  radius <- diameter / 2
+  xData <- origin[1] + radius * cos(angleSeq)
+  yData <- origin[2] + radius * sin(angleSeq)
+  return(data.frame(x = xData, y = yData, group = group))
 }
 
-drawMan <- function(st_napak) { #, iskana_beseda) {
-  ggplot(levels[which(levels$group <= st_napak), ], aes(x = x, y = y, group = group)) + 
-    geom_path(size = 2.5) + 
+# Function to draw the hangman based on the number of wrong attempts
+drawHangman <- function(wrongAttempts) {
+  ggplot(levels[levels$group <= wrongAttempts, ], aes(x = x, y = y, group = group)) +
+    geom_path(linewidth = 2.5) + # Fixed the warning by using linewidth
     theme_void() +
     ggtitle('Hangman Game')
 }
 
+# Function to check if the letter has been guessed already
+checkDuplicateGuess <- function(letter, guesses) {
+  if (length(guesses) == 0) {
+    message("Starting the game! Checking your guess...")
+    guesses <- rbind(guesses, guess = letter)
+    guesses[, 1] <- as.character(guesses[, 1])
+  } else {
+    if (grepl(letter, guesses) == TRUE) {
+      guesses[, 1] <- as.character(guesses[, 1])
+      message("Already guessed... Try another letter!")
+    } else {
+      guesses <- rbind(guesses, guess = letter)
+      guesses[, 1] <- as.character(guesses[, 1])
+      message("Checking your guess...")
+    }
+  }
+  return(guesses)
+}
 
-CheckDuplicate <- function(crka, izbor){
-  if (length(izbor) == 0) {
-    message("Start up the game! Checking your guess...")
-    izbor <- rbind(izbor, izb=crka)  
-    izbor[,1] <- as.character(izbor[,1])
-} else {
-    if (grepl(crka,izbor) == TRUE) {
-    izbor[,1] <- as.character(izbor[,1])
-    message("Already exists... Another guess!")
-} else {
-    izbor <- rbind(izbor, izb=crka)
-    izbor[,1] <- as.character(izbor[,1])
-    message("Checking your guess...")
-}
-}
-  return(izbor)
-}
-
-#Data for graph
-level1 <- data.frame(x = c(1, 2, 3, 4, 5, 6, 7, 8), 
-                     y = c(1, 1, 1, 1, 1, 1, 1, 1), 
+# Data for graph
+level1 <- data.frame(x = c(1, 2, 3, 4, 5, 6, 7, 8),
+                     y = c(1, 1, 1, 1, 1, 1, 1, 1),
                      group = c(1, 1, 1, 1, 1, 1, 1, 1))
-level2 <- data.frame(x = c(4, 4, 4, 4, 4), 
+level2 <- data.frame(x = c(4, 4, 4, 4, 4),
                      y = c(1, 2, 3, 4, 5),
                      group = c(2, 2, 2, 2, 2))
-level3 <- data.frame(x = c(4, 5, 6), y= c (5, 5, 5), group = c(3, 3, 3))
+level3 <- data.frame(x = c(4, 5, 6), y = c(5, 5, 5), group = c(3, 3, 3))
 level4 <- data.frame(x = c(6, 6), y = c(5, 4), group = c(4, 4))
-level5 <- drawHead(c(6, 3.5), 1, 10, 5)
-level6 <- data.frame(x = c(6, 6, 5.8, 6.2), 
-                     y =c(3, 1.5, 1.5, 1.5), group = c(6, 6, 6, 6))
+level5 <- drawHead(c(6, 3.5), 1, 100, 5)
+level6 <- data.frame(x = c(6, 6, 5.8, 6.2),
+                     y = c(3, 1.5, 1.5, 1.5), group = c(6, 6, 6, 6))
 level7 <- data.frame(x = c(5.5, 6, 6.5), y = c(2, 2.5, 2), group = c(7, 7, 7))
 levels <- rbind(level1, level2, level3, level4, level5, level6, level7)
 rm(level1, level2, level3, level4, level5, level6, level7)
 
-#Helper variables
-suppressWarnings(rm(st_napak, izbor, crka, cilj, cilj_n, iskana_beseda, beseda, i, active))
-st_napak = 0
-i = 0
-izbor = data.frame(izb=c(NULL))
-cilj = NULL
-cilj_n = data.frame(izb=c(NULL))
-active = TRUE
+# Helper variables
+suppressWarnings(rm(wrongAttempts, guesses, letter, targetWord, targetLetters, guessedWord, word, attempts, isActive))
+wrongAttempts <- 0
+attempts <- 0
+guesses <- data.frame(guess = c(NULL))
+targetWord <- NULL
+targetLetters <- data.frame(guess = c(NULL))
+isActive <- TRUE
 
-#Hangman
-StartNewGame <- function(sensitive.flag = TRUE) { # sensitive.flag: TRUE -> capital letters are available. 
-  beseda <- readline(prompt = "Word: ")
+# Function to start a new game
+startNewGame <- function(caseSensitive = TRUE) {
+  word <- readline(prompt = "Enter the word: ")
+  cat("\f")
+  graphics.off()
+  if (caseSensitive == FALSE) {
+    word <- tolower(word)
+  }
   
-  cat("\f") 
-  graphics.off() 
-  if (sensitive.flag == FALSE) {
-    beseda <- base::tolower(beseda)
-}
+  guessedWord <- replicate(nchar(word), '_')
   
-  iskana_beseda <- replicate(nchar(beseda), '_')
-  
-  while (active == TRUE) {
-    
-    if (i == 0) {
-    writeLines(paste(iskana_beseda, collapse = " "))
-}
-    
-    crka <- readline(prompt="Enter Letter: ")
-    
-    if (nchar(crka)>1) message("Taking first letter")
-    crka <- substr(crka, 1, 1)
-    
-    izbor <- CheckDuplicate(crka, izbor)
-    
-    #iskana_beseda
-    if (grepl(crka, beseda) == TRUE) {
-      
-    cilj <- rbind(cilj, crka)
-    iskana_beseda <- zamenjaj2(beseda, crka, iskana_beseda)
-       
-    message(paste("Good guess!","Number of attempts:",i+1)) 
-      
-    if (as.character(paste(base::tolower(iskana_beseda), collapse = "")) == base::tolower(beseda)) {
-    active == FALSE
-    message("Bravo, victory!")
-    break
-}
-      
-}   else {
-    cilj_n <- CheckDuplicate(crka=crka, izbor=cilj_n)
-    message(paste("Nope!", "Number of attempts:", i + 1, "     ", "Wrong letters: {", (toString(paste0(cilj_n[,1]))), "}")) 
-    st_napak <- as.integer(nrow(cilj_n))
-    print(drawMan(st_napak = st_napak))#,iskana_beseda=paste(iskana_beseda, collapse =  " ") ))
-    
-    if(as.integer(st_napak) == 7){
-      active == FALSE
-      break
-      message("Game Over...")
+  while (isActive == TRUE) {
+    if (attempts == 0) {
+      writeLines(paste(guessedWord, collapse = " "))
     }
     
-}
+    letter <- readline(prompt = "Enter a letter: ")
     
-    i= i + 1
+    if (nchar(letter) > 1) message("Taking the first letter")
+    letter <- substr(letter, 1, 1)
     
-    if(st_napak == 7){
-      active == FALSE
-      break
-      message("Game Over...")
+    guesses <- checkDuplicateGuess(letter, guesses)
+    
+    if (grepl(letter, word) == TRUE) {
+      targetLetters <- rbind(targetLetters, letter)
+      guessedWord <- updateGuessedWord(word, letter, guessedWord)
+      message(paste("Good guess!", "Number of attempts:", attempts + 1))
+      
+      if (paste(tolower(guessedWord), collapse = "") == tolower(word)) {
+        isActive <- FALSE
+        message("Congratulations, victory!")
+        break
+      }
+      
+    } else {
+      targetLetters <- checkDuplicateGuess(letter, targetLetters)
+      message(paste("Nope!", "Number of attempts:", attempts + 1, "Wrong letters: {", toString(paste0(targetLetters[, 1])), "}"))
+      wrongAttempts <- as.integer(nrow(targetLetters))
+      print(drawHangman(wrongAttempts))
+      
+      if (wrongAttempts == 7) {
+        isActive <- FALSE
+        message("Game Over... You lost!")
+        break
+      }
     }
+    attempts <- attempts + 1
   }
 }
 
-#Start new Game
-StartNewGame()
+# Start a new game
+startNewGame()
